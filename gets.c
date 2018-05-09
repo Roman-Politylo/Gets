8e2db40ef3e13acb687b5a53c90a4ad99a825a45
Roman <roman.politylo777@gmail.com>
2018-05-09 12:27:29 -0400

    First edition.

---
+++ gets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+char s[30];
+    puts("Give me sentense: ");
+    gets(s);
+    puts("Thank you for: ");
+    puts(s);
+}
